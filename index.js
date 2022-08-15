885bb348f3302576b43d58ed4ce9217c174781c2
danielpdaniel <artofjquinn@gmail.com>
2022-08-15 16:18:49 -0400

    commit tree 9c1ae0a90969d97c3ccc1f0883269da6f90a2a15

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     console.log(gifts);
+// }
+
+function writeCards(array, eventName) {
+    let newArray = [...array];
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(integer) {
+while (integer >= 0) {
+    console.log(integer);
+    integer--;
+    }
+}
+
+// writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+// wrapGifts(gifts);
